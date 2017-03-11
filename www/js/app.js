f4fd49d637e5ba79a432b9f308b9257b7797ad44
GabrielePrestifilippo <gabry.501@live.it>
2017-03-11 08:52:08 +0100

    integrated profile and rewards page

--- www/js/app.js
+++ www/js/app.js
@@ -10,9 +10,11 @@ document.addEventListener('init', function(event) {
   }
 
 });
-
+var firstMap=false;
+var firstProfile=false;
 document.addEventListener("show", function (event) {
-  if (event.target.id == "reward") {
+  if (event.target.id == "reward" && !firstMap) {
+    firstMap=true;
     var mymap = L.map('mapid').setView([47.3893396,8.5184537], 15);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
       maxZoom: 18,
@@ -43,5 +45,89 @@ document.addEventListener("show", function (event) {
     L.marker([47.38924,8.51422], {icon: shop}).addTo(mymap).bindPopup("Shop2");
     L.marker([47.38722,8.51824], {icon: shop}).addTo(mymap).bindPopup("Shop3");
     L.marker([47.39031,8.52017], {icon: shop}).addTo(mymap).bindPopup("Shop4");
+
+  }else if(event.target.id == "profile" && !firstProfile){
+    firstProfile=true;
+    var w = 150,
+        h = 150;
+
+    var colorscale = d3.scale.category10();
+
+//Legend titles
+    var LegendOptions = ['Shopping preferences'];
+
+//Data
+    var d = [
+      [
+        {axis:"Casual Clothing",value:0.59},
+        {axis:"Sport Clothing",value:0.56},
+        {axis:"Business Clothing",value:0.42},
+        {axis:"Electronics",value:0.34},
+        {axis:"Groceries",value:0.48},
+        {axis:"Coffee, Bakery",value:0.14}
+      ]
+    ];
+
+//Options for the Radar chart, other than default
+    var mycfg = {
+      w: w,
+      h: h,
+      maxValue: 0.6,
+      levels: 6,
+      ExtraWidthX: 300
+    }
+
+//Call function to draw the Radar chart
+//Will expect that data is in %'s
+    RadarChart.draw("#chart", d, mycfg);
+
+////////////////////////////////////////////
+/////////// Initiate legend ////////////////
+////////////////////////////////////////////
+
+    var svg = d3.select('#body')
+        .selectAll('svg')
+        .append('svg')
+        .attr("width", w+300)
+        .attr("height", h)
+
+//Create the title for the legend
+    var text = svg.append("text")
+        .attr("class", "title")
+        .attr('transform', 'translate(90,0)')
+        .attr("x", w - 70)
+        .attr("y", 10)
+        .attr("font-size", "12px")
+        .attr("fill", "#404040");
+
+//Initiate Legend
+    var legend = svg.append("g")
+            .attr("class", "legend")
+            .attr("height", 100)
+            .attr("width", 200)
+            .attr('transform', 'translate(90,20)')
+        ;
+//Create colour squares
+    legend.selectAll('rect')
+        .data(LegendOptions)
+        .enter()
+        .append("rect")
+        .attr("x", w - 65)
+        .attr("y", function(d, i){ return i * 20;})
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function(d, i){ return colorscale(i);})
+    ;
+//Create text next to squares
+    legend.selectAll('text')
+        .data(LegendOptions)
+        .enter()
+        .append("text")
+        .attr("x", w - 52)
+        .attr("y", function(d, i){ return i * 20 + 9;})
+        .attr("font-size", "11px")
+        .attr("fill", "#737373")
+        .text(function(d) { return d; })
+    ;
   }
 }, false);
